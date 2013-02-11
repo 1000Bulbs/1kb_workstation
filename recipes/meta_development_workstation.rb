1485260cf9d1d75f9b6126d01fd935cecea572f5
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2013-02-11 07:55:58 -0600

    Add warning

--- recipes/meta_development_workstation.rb
+++ recipes/meta_development_workstation.rb
@@ -3,4 +3,6 @@ include_recipe "pivotal_workstation::mysql"
 include_recipe "pivotal_workstation::mongodb"
 include_recipe "pivotal_workstation::postgres"
 include_recipe "pivotal_workstation::redis"
+
+# this must be run last, because it does dumb stuff with permissions in /usr/local
 include_recipe "1kb_workstation::php53"
