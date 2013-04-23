0c2f8f2d7032d34ed6b8a2f44d3e86a3c8a974ec
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2013-04-23 06:06:14 -0500

    Add pivotal_workstation dependency

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ description       "Configure frequently-used tools for an OSX workstation"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
 supports          "mac_os_x"
+
+depends 'pivotal_workstation'
